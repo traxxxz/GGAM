88728644ffbe5d4f1d34d76f1052c87d5a3632b8
traxxxz <161995432+traxxxz@users.noreply.github.com>
2025-10-18 13:33:56 +0300

    Initial commit

---
+++ # Code Citations.md
@@ -0,0 +1,11 @@
+# Code Citations
+
+## License: MIT
+- https://github.com/alibaba/ice/blob/37c4278b13b1dbb19dfba174642f61f8be97da48/packages/vite-plugin-index-html/tests/index.test.ts
+- https://github.com/thought7878/thought7878.github.io/blob/3658eb6f7a30aec644fd309cdac6aedf9fa8d230/_posts/component-lib/antd/0.antd.md
+
+## License: unknown
+- https://github.com/harshithkrpt/nodejs/blob/7c76c5d4e837280ec2cb762e785667856df3b2a0/web-server/templates/views/index.hbs
+- https://github.com/iamsouliya/The-Dojo/blob/5d4576261370bd122b9f204e4ab05ddde2fdb141/index.html
+- https://github.com/paul10116/Stock-Market/blob/71df82ca7c8a43b51f074da5518aab6af5e1df0a/web.app/index.html
+- https://github.com/dealoux/practice_webapp_frontend/blob/63d7b8140a8f5b1eb9337d4e02af5565d9951d77/index.html
--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,44 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    server: {
+      port: 3000,
+      host: true,
+      strictPort: true
+    },
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: 'autoUpdate'
+      })
+    ],
+    define: {
+      'process.env': JSON.stringify(env)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        'assets': path.resolve(__dirname, './src/assets'),
+        'styles': path.resolve(__dirname, './src/styles')
       }
-    };
+    },
+    build: {
+      sourcemap: true,
+      outDir: 'dist',
+      minify: 'terser',
+      rollupOptions: {
+        output: {
+          assetFileNames: 'assets/[name].[hash][extname]',
+          chunkFileNames: 'js/[name].[hash].js',
+          entryFileNames: 'js/[name].[hash].js'
+        }
+      },
+      assetsInlineLimit: 4096
+    }
+  };
 });
